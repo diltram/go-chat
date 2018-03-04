2ad6f5e3de806876f2b70258f5b3047ec59546a2
Lubosz "diltram" Kosnik <diltram@gmail.com>
2018-03-03 21:47:30 -0800

    Configuration of a server

---
+++ internal/app/go-chat/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"io/ioutil"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	defaultIP   = "127.0.0.1"
+	defaultPort = 5000
+	defaultLog  = "logs/go-chat.log"
+)
+
+//Server configuration structure.
+type Server struct {
+	//IP speficies address on which server will listen
+	IP string `yaml:"ip"`
+	//Port specifies port on which server will listen
+	Port uint16 `yaml:"port"`
+	//Log points to exact file which will be used to store all logs
+	Log string `yaml:"log"`
+}
+
+//Configuration structure which handle complete settings.
+type Configuration struct {
+	//Server holds configuration specific for telnet server
+	Server Server `yaml:"server"`
+}
+
+//NewDefaultConfig initializes configuration with default values.
+func NewDefaultConfig() Configuration {
+	return Configuration{
+		Server{
+			IP:   defaultIP,
+			Port: defaultPort,
+			Log:  defaultLog,
+		},
+	}
+}
+
+func loadConfig(filename string) Configuration {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Infof("Couldn't open %s configuration file. Using default settings.", filename)
+		return NewDefaultConfig()
+	}
+
+	var c Configuration
+	err = yaml.Unmarshal(bytes, &c)
+	if err != nil {
+		log.Infof("Couldn't read %s configuration file. Err: %s. Using default settings.", filename, err)
+		return NewDefaultConfig()
+	}
+
+	return c
+}
---
+++ internal/app/go-chat/config/config_test.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"path/filepath"
+	"testing"
+	//"github.com/diltram/go-chat/test/fixtures/file"
+)
+
+//TestLoadConfig creates configuration based on the test file.
+//All parameters should be correctly loaded and set.
+func TestLoadConfig(t *testing.T) {
+	expected := Configuration{
+		Server: Server{
+			IP:   "0.0.0.0",
+			Port: 5555,
+			Log:  "logs/go-chat.log",
+		},
+	}
+	conf := loadConfig(filepath.Join("testdata", "config.yml"))
+
+	if conf != expected {
+		t.Errorf("Generated config is different. Expected: %+v, got: %+v", expected, conf)
+	}
+}
+
+//TestLoadConfigNoFile tests default config as file doesn't exist.
+func TestLoadConfigNoFile(t *testing.T) {
+	expected := NewDefaultConfig()
+	conf := loadConfig("non_existing_path")
+
+	if conf != expected {
+		t.Errorf("Generated config is different. Expected: %+v, got: %+v", expected, conf)
+	}
+}
+
+//TestLoadConfigNoFile tests default config as file doesn't exist.
+func TestLoadConfigCorruptedData(t *testing.T) {
+	expected := NewDefaultConfig()
+	conf := loadConfig(filepath.Join("testdata", "corrupted_config.yml"))
+
+	if conf != expected {
+		t.Errorf("Generated config is different. Expected: %+v, got: %+v", expected, conf)
+	}
+}
